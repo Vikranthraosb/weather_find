daf3ad82c493944b4f0fb9e3cda6273a86b1b4ea
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2024-01-03 19:38:14 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express= require('express')
+const app= express();
+const path =require('path')
+const port = process.env.PORT || 3000   // dynamically assigns the value of PORT from the environment variables if available; otherwise, defaults to port 3000, ensuring adaptability when hosted in different environments.
+
+// public static path;
+console.log(path.join(__dirname, "../public"))
+
+// app.use(express.static());
+
+
+
+//routing
+app.get("/", (req,res)=>{
+    res.send("welcome to my project")
+})
+app.get("/about", (req,res)=>{
+    res.send("welcome to my about")
+})
+app.get("/weather", (req,res)=>{
+    res.send("weather channel")
+})
+app.get("*", (req,res)=>{
+    res.send("404 errroe created by me")
+})
+
+app.listen(port,()=>{
+    console.log(`listning to the port at ${port}`)
+})
---
+++ steps.md
@@ -0,0 +1,4 @@
+1. create file structure and install file structure ( express generator not used here)
+2. npm i express, requirre it in app.js and write port
+3. To run, 'npx nodemon app.js' ( because app.js is inside src, so default nodemon cant find it )
+4. make routs
