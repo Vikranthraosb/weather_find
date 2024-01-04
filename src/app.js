6c5106cf2b8fee679de22143a33ff577a21a80f9
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2024-01-05 00:04:27 +0530

    hbs completed .

--- src/app.js
+++ src/app.js
@@ -4,12 +4,13 @@ const path =require('path')
 const port = process.env.PORT || 3000   // dynamically assigns the value of PORT from the environment variables if available; otherwise, defaults to port 3000, ensuring adaptability when hosted in different environments.
 const hbs =require('hbs')
 // public static path;
-const static_path=(path.join(__dirname, "../public"))
+const static_path=(path.join(__dirname, "../public")) // these are must need
+const path2=(path.join(__dirname, "../views"))
 
 app.set('view engine', 'hbs');
 app.use(express.static(static_path));
 app.set('views', path.join(__dirname, '../views')); // this is must needed to define the path
-
+hbs.registerPartials(path2)
 
 //routing
 app.get("/", (req,res)=>{
