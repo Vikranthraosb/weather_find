e5295e4c4749cafc58db691a4680ec1cacbe2f25
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2024-01-05 23:50:31 +0530

    api partially added

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+const subbtn= document.querySelector("#submitbtn")
+const getinfo = ()=>{
+    alert("hi");
+let url = api.openweathermap.org/data/2.5/weather?q=pune&appid=;
+}
+subbtn.addEventListener("click",getinfo);
