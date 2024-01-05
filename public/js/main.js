de2fcd7ae4fc670c493c87d795aedb74f3dc21b1
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2024-01-06 00:21:53 +0530

    some error in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,28 @@
-const subbtn= document.querySelector("#submitbtn")
-const getinfo = ()=>{
-    alert("hi");
-let url = api.openweathermap.org/data/2.5/weather?q=pune&appid=;
+const cityname =document.querySelector("#cityname");
+const subbtn= document.querySelector("#submitbtn");
+const city_name =document.querySelector("#city_name");
+
+
+ const getinfo = async (event)=>{
+    let cityval=cityname.Value;
+
+    alert("hmm");
+
+    event.prevetDefault();
+if(cityval == ""){
+    city_name.innerHTML = "Please Enter Name of city";
+}
+
+else{
+  try{
+    let url = `api.openweathermap.org/data/2.5/weather?q=pune&appid=d5359962e6c01ae89915cd260d92c425`;
+    const response = await fetch(url)
+    
+  }  
+  catch{
+    city_name.innerHTML = `Please Enter Name of city`
+
+  }
+}
 }
 subbtn.addEventListener("click",getinfo);
